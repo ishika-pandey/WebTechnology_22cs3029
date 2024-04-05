a8c158e3e92a03f7176c491a1e0401968fbf48cb
Mayank Bharti <22cs3039@gmail.com>
2024-04-05 23:50:41 +0530

    changes in project

--- Backend_webd/src/app.js
+++ Backend_webd/src/app.js
@@ -7,21 +7,17 @@ require("./db/conn");
 const port =process.env.port || 3000;
 
 const static_path = path.join(__dirname,"../public");
-const template_path = path.join(__dirname,"../templates/views");
-//const partials_path = path.join(__dirname,"../templates/partials");
-
-//app.use("/images", express.static(path.join(__dirname, "/public")));
 app.use(express.static(static_path));
-// app.set("view engine","hbs")
-// app.set("views",template_path);
-// hbs.registerPartials(partials_path);
-
+app.use("/images", express.static(path.join(__dirname, "../public")));
 
 app.get("/",(req,res)=>{
-    res.sendFile("index.html")
+    res.sendFile("/C:\Users\Mayank bharti\Downloads\web d\Backend_webd\public\index.html");
 
 });
+app.get("/images",(req,res)=>{
+    res.sendFile("/C:\Users\Mayank bharti\Downloads\web d\Backend_webd\public\images\home.jpg");
 
+});
 app.listen(port,()=>{
     console.log(`server is running at port no ${port}`)
     
